c792c9b59bcb135dec012e5328bc2db7496784b3
Griff209 <nicholas.s.griffen@gmail.com>
2018-05-21 18:25:05 -0700

    Update 1_multiples.js

--- 1_multiples.js
+++ 1_multiples.js
@@ -1,7 +1,7 @@
 var multiples = [];
 var i = 999;
 while (i > 0) {
-  var isMultiple = (i % 5) === 0 || (i % 3) === 0;
+  var isMultiple = (i % 3) === 0 || (i % 5) === 0;
   if (isMultiple) {
     multiples.push(i);
   } 
