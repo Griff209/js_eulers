1369e879bccf7f724b424319a1b1a22cb56e72e9
Griff209 <nicholas.s.griffen@gmail.com>
2018-06-23 18:16:09 -0700

    Create palindrome.js

---
+++ palindrome.js
@@ -0,0 +1,11 @@
+
+function isPalindrome(number) {
+  let digitArray = Array.isArray(number) ? number.slice() : String(number).split("");
+  if (digitArray.length === 1 || digitArray.length === 0) {
+    return true;
+   } else if (digitArray.pop() === digitArray.shift()) {
+     return isPalindrome(digitArray);
+   } else {
+     return false;
+   }
+}
