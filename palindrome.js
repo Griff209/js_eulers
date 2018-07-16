2f61f2e0e566d28b4c0218ded11d94d208313e91
Griff209 <nicholas.s.griffen@gmail.com>
2018-07-16 16:30:57 -0700

    Experiment with style
    Explore recent reading on `Automatic Semicolon Insertion`

--- palindrome.js
+++ palindrome.js
@@ -1,10 +1,8 @@
 function isPalindrome(number) {
-  let digitArray = Array.isArray(number) ? number.slice() : String(number).split("");
-  if (digitArray.length === 1 || digitArray.length === 0) {
-    return true;
-   } else if (digitArray.pop() === digitArray.shift()) {
-     return isPalindrome(digitArray);
-   } else {
-     return false;
-   }
+  let digitArray = Array.isArray(number) ? number.slice() : String(number).split("")
+  ;if (digitArray.length === 1 || digitArray.length === 0) 
+    return true
+  if (digitArray.pop() === digitArray.shift()) 
+    return isPalindrome(digitArray)
+  return false
 }
